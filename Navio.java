f3af4f757a1a67c7e13cffae99651b2df42cbe7f
VitorCosta8102 <vitor.formando.eletrica@gmail.com>
2018-07-22 13:22:42 -0300

    Implementação da maioria dos metodos da classe Navio e dos atributos da classe Celula apenas para o teste dos metodos da Classe Navio.

--- Navio.java
+++ Navio.java
@@ -1,32 +1,35 @@
-public abstract class Navio {
+public class Navio {
 	private String tipoNavio;
-	private boolean afundado;
 	private Celula[] celulasOcupadas;
 	private int celulasAtivas;
 	
-	public Navio(String tipoNavio, int numeroCeluasOcupadas){
-		
+	public Navio(String tipoNavio, int numeroCeluasOcupadas) {
+		inicializaNavio(tipoNavio,numeroCeluasOcupadas);
 	}
 	//metodo para diminuir o numero de celulas que ainda podem receber tiro
 	public void decrementaCelulasAtivas(){
-		
+		celulasAtivas--;
 	}
 	
-	public void setAfundado(){
-		
+	public void inicializaNavio(String tipoNavio, int numeroCeluasOcupadas) {
+		this.tipoNavio=tipoNavio;
+		celulasOcupadas=new Celula[numeroCeluasOcupadas];
+		celulasAtivas=numeroCeluasOcupadas;
 	}
 	
 	public boolean checaAfundado(){
-		
+		if(celulasAtivas==0)
+			return true;
+		return false;
 	}
 	
 	public String getTipo(){
-		
+		return tipoNavio;
 	}
 	
-	public void posicionaNavio(int linha, int coluna, char orientacao){
+	/*public void posicionaNavio(int linha, int coluna, char orientacao){
 		
-	}
+	}*/
 	
 	
 }
