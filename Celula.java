924d8479d0f71f85c9ecfde5e5ac9fde3ae39598
VitorCosta8102 <vitor.formando.eletrica@gmail.com>
2018-07-22 18:36:56 -0300

    Completando a classe Celula

--- Celula.java
+++ Celula.java
@@ -1,33 +1,38 @@
-public class Celula {
+public class Celula implements Imprimivel{
 	private String nuvem="oOo";
 	private String conteudo;
 	private boolean tiro;
 	
 	public Celula(){
-	
+		tiro=false;
 	}
 	
 	public void setConteudo(String conteudo){
-		
+		this.conteudo=conteudo;
 	}
 	
 	public String getConteudo(){
-		
+		return conteudo;
 	}
 	
 	public boolean getTiro(){
-		
+		return tiro;
 	}
 	
 	public void setTiro(){
-		
+		if(conteudo!="~~~")
+			conteudo=" X ";
+		tiro=true;
 	}
 	
 	public String getNuvem(){
-		
+		return nuvem;
 	}
-	
+	@Override
 	public void imprimir(){
-	
+		if(tiro)
+			System.out.print(getConteudo());
+		else
+			System.out.print(getNuvem());
 	}
 }
