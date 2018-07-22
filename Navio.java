7de6231fa392fb57f17118f5d7fc066b52f2c1d3
VitorCosta8102 <vitor.formando.eletrica@gmail.com>
2018-07-22 09:27:21 -0300

    Estrutura dos metodos da classe navio adicionada.

---
+++ Navio.java
@@ -0,0 +1,32 @@
+public abstract class Navio {
+	private String tipoNavio;
+	private boolean afundado;
+	private Celula[] celulasOcupadas;
+	private int celulasAtivas;
+	
+	public Navio(String tipoNavio, int numeroCeluasOcupadas){
+		
+	}
+	//metodo para diminuir o numero de celulas que ainda podem receber tiro
+	public void decrementaCelulasAtivas(){
+		
+	}
+	
+	public void setAfundado(){
+		
+	}
+	
+	public boolean checaAfundado(){
+		
+	}
+	
+	public String getTipo(){
+		
+	}
+	
+	public void posicionaNavio(int linha, int coluna, char orientacao){
+		
+	}
+	
+	
+}
