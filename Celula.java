ef1249e283a9a0521f259c1c57212149ed8cfcb4
VitorCosta8102 <vitor.formando.eletrica@gmail.com>
2018-07-22 14:51:55 -0300

    Estrutura celula feita

--- Celula.java
+++ Celula.java
@@ -3,5 +3,31 @@ public class Celula {
 	private String conteudo;
 	private boolean tiro;
 	
+	public Celula(){
 	
+	}
+	
+	public void setConteudo(String conteudo){
+		
+	}
+	
+	public String getConteudo(){
+		
+	}
+	
+	public boolean getTiro(){
+		
+	}
+	
+	public void setTiro(){
+		
+	}
+	
+	public String getNuvem(){
+		
+	}
+	
+	public void imprimir(){
+	
+	}
 }
