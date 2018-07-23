2962e4478468150c4e240c9ee2812782c29b66b3
VitorCosta8102 <vitor.formando.eletrica@gmail.com>
2018-07-23 11:39:11 -0300

    Estrutura da classe Tabuleiro

---
+++ Tabuleiro.java
@@ -0,0 +1,30 @@
+public class Tabuleiro {
+	private Celula[][] tabuleiro;
+	private Navio[] navios;
+	private int numeroNavios;
+	
+	public Tabuleiro (int tamanho){
+	
+	}
+	
+	public void inicializaCelulas(){
+	
+	}
+	
+	public void inicializaNavios(){
+	
+	}
+	
+	public void posicionaNavios(int linha, int coluna, char posicao){
+	
+	}
+	
+	public void procuraAlvo(){
+	
+	}
+	
+	public Celula getCelula(int linha, int coluna){
+	
+	}
+	
+}
