c2168e5b06b2601413d47157943aea1f32b22c73
VitorCosta8102 <vitor.formando.eletrica@gmail.com>
2018-07-28 19:16:48 -0300

    Classe principal ajustada

--- Principal.java
+++ Principal.java
@@ -2,18 +2,10 @@ import java.util.Scanner;
 
 public class Principal {
 	public static void main(String[] args){
+		
 		BatalhaNaval jogo=new BatalhaNaval();
 		jogo.jogar();
-		/*
-		jogo.posicionaNaviosTabuleiros();
-		int i=0;
-		while(!jogo.checaFimDeJogo()){ //Teste dos tiros
-			jogo.darTiro(1);
-			jogo.imprimir(1);
-			jogo.darTiro(2);
-			jogo.imprimir(2);
-			i++;
-		}*/
+		
 	}
 }
 
