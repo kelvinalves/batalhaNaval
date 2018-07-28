ed513ed18ed6baeda726ac9576d9f124424a6730
kelvinalves <0kelvin.alves@gmail.com>
2018-07-28 19:13:09 -0300

    Correção do método darTiroNaCelula

--- BatalhaNavalComputador.java
+++ BatalhaNavalComputador.java
@@ -1,24 +1,24 @@
-﻿import java.util.Random;
+import java.util.Random;
 
 public class BatalhaNavalComputador extends BatalhaNaval {
-	private int sorteio;
+	private int sorteio, tamanho;
 	private char orientacao;
-	private Celula celulaAnterior = "~~~";
-	
+	private String celulaAnterior;
+
 	public int aleatorio(){
 		//instancia um objeto da classe Random usando o construtor básico
 		Random gerador = new Random();
 
-		int tamanho = getTabuleiro(1).tamanhoTabuleiro;
-	
+		this.tamanho = getTabuleiro(1).tamanhoTabuleiro();
+
 		//sequência entre 0 e tamanho + 1
 
-		int sorteio = gerador.nextInt(tamanho);
-		
+		this.sorteio = gerador.nextInt(tamanho);
+
 		return sorteio;
 	}
 	public char orientacao(){
-				
+
 		if (aleatorio() < ((tamanho)/2))
 			orientacao = 'H';
 		else
@@ -26,27 +26,29 @@ public class BatalhaNavalComputador extends BatalhaNaval {
 		return orientacao;
 	}
 	@Override
-	public void darTiro(aleatorioIndice(), aleatorioIndice(), 2) throws Exception{
-		if(celulaAux.getConteudo().equals(" X "))
+	public void darTiroNaCelula(int coluna, int linha, Tabuleiro tabuleiro) throws Exception{
+		if(celula.getConteudo().equals(" X "))
 			celulaAnterior = " X ";
 	}
 	public void tipoTiro(Celula celulaAnterior){
-		if(celulaAnterior.getConteudo().equals(" X "))
+		/*if(celulaAnterior.getConteudo().equals(" X "))
 			darTiroInteligente();
-		else
-			darTiro(); 
-		
+		else*/
+			darTiroNaCelula(aleatorio(), aleatorio(), tabuleiro2);
+
 	}
-	
+	/*
 	public class darTiroInteligente(){
 		itens = new Object[3]; // as quatro posições da cruz
 		for(int i = 0; i<=3; i++){
-			
-			itens[i] = 
+
+			itens[i] =
 
 	}
 
-	@Override	
+	@Override
 	public void posicionaNavio(aleatorioIndice(), aleatorioIndice(), orientacao, Celula[][] tabuleiro)
 
-	}
+	}*/
+}
+
