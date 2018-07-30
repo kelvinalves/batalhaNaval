533f6e27fc1b61a864874eae107af48c94c6970a
kelvinalves <32200228+kelvinalves@users.noreply.github.com>
2018-07-30 12:29:59 -0300

    Formatação

--- BatalhaNavalComputador.java
+++ BatalhaNavalComputador.java
@@ -5,46 +5,47 @@ public class BatalhaNavalComputador extends BatalhaNaval {
 	private char orientacao;
 	private String celulaAnterior = "~~~";
 
-	public int aleatorio() {
-    Random gerador = new Random();
-    tamanho = getTabuleiro(1).tamanhoTabuleiro();
-    sorteio = gerador.nextInt(tamanho);
-    return sorteio;
+	public int aleatorio(){
+		Random gerador = new Random();
+		tamanho = getTabuleiro(1).tamanhoTabuleiro();
+		sorteio = gerador.nextInt(tamanho);
+		return sorteio;
 	}
-	public char orientacao() {
-    if (aleatorio() < ((tamanho)/2))
+	public char orientacao(){
+		if(aleatorio()<((tamanho)/2))
 			orientacao = 'H';
 		else
 			orientacao = 'V';
 		return orientacao;
 	}
 	@Override
-	public void darTiroNaCelula(int linha, int coluna, Tabuleiro tabuleiro) {
-		Celula celula=tabuleiro.getCelula(linha,coluna);
+	public void darTiroNaCelula(int linha, int coluna, Tabuleiro tabuleiro){
+		Celula celula = tabuleiro.getCelula(linha,coluna);
 		if(celula.getTiro())
-		  celula.setTiro();
-		if(celula.getConteudo().equals(" X "))
+			celula.setTiro();
+		if(celula.getConteudo().equals(" X ")){
 			tabuleiro.atiraNoNavio(celula);
 			celulaAnterior = " X ";
+		}
 	}
 	public void tipoTiro(){
 		if(celulaAnterior == " X ")
 			darTiroInteligente(linha, coluna);
-		else {
-      this.linha = aleatorio();
-      this.coluna = aleatorio();
-      darTiroNaCelula(linha, coluna, tabuleiro2);
-    }
+		else{
+			this.linha = aleatorio();
+			this.coluna = aleatorio();
+			darTiroNaCelula(linha, coluna, tabuleiro2);
+		}
 	}
-	public void darTiroInteligente(int linha, int coluna) {
-    int a = aleatorio();
-    if (a < tamanho/4)
-		  darTiroNaCelula(linha-1, coluna, tabuleiro2);
+	public void darTiroInteligente(int linha, int coluna){
+		int a = aleatorio();
+		if (a < tamanho/4)
+			darTiroNaCelula(linha-1, coluna, tabuleiro2);
 		else if (a < tamanho/2)
 			darTiroNaCelula(linha+1, coluna, tabuleiro2);
-    else if (a >= tamanho/2)
+		else if (a >= tamanho/2)
 			darTiroNaCelula(linha, coluna-1, tabuleiro2);
-    else
+		else
 			darTiroNaCelula(linha, coluna+1, tabuleiro2);
 	}
   /*
